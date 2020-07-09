962446521805b5a9ffdea29bf6da8c04d81b6e06
Fido García <garciafido@gmail.com>
2020-07-09 20:12:00 -0300

    Click events and colored function

--- src/components/ArgentinaMap/component.tsx
+++ src/components/ArgentinaMap/component.tsx
@@ -5,10 +5,9 @@ function ArgentinaMap() {
   return (
     <div className="ArgentinaMap">
         <Argentina width={"700"} height={"700"}
-                   mendoza={"#FFF690"}
-                   malvinas={"#FFF690"}
-                   antartida={"#FFF690"}
-                   antartida_out={"#C7BDC6"}
+                   getColor={(provincia) => "#fff"}
+                   clicked={(provincia) => alert(provincia)}
+                   out={"#C7BDC6"}
                    sea={"#90d8ff"} />
     </div>
   );
